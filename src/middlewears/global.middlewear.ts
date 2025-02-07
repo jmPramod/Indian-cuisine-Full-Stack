eb3552337f6e25cc7f6dd96d23cefd2549f4be5f
Pramod JM <pramodjm4@gmail.com>
2025-02-07 13:30:02 +0530

    add stack error

--- src/middlewears/global.middlewear.ts
+++ src/middlewears/global.middlewear.ts
@@ -9,6 +9,7 @@ export const errorHandler: ErrorRequestHandler = (err: any, req: Request, res: R
     error: err.message||"Something went wrong",
     message:null,
     status:err.status||500,
-    data:null
+    data:null,
+    stacks: err.stack,
   });
 };
